9681ed015849035b938e4b831df8fbeb73676394
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-12-24 11:12:06 -0800

    refactor: fix multiple clearTimeout issue

--- scripts.js
+++ scripts.js
@@ -10,91 +10,87 @@ class Clapper {
         this.mouseDown = false
         this.lastClap = Date.now()
 
-        this.increaseClapCounter = this.increaseClapCounter.bind(this)
-        this.removeClapAnimations = this.removeClapAnimations.bind(this)
-        this.setClapAnimations = this.setClapAnimations.bind(this)
-        this.setHandsClapped = this.setHandsClapped.bind(this)
-        this.setClapCounts = this.setClapCounts.bind(this)
-        this.startClaps = this.startClaps.bind(this)
-        this.stopClaps = this.stopClaps.bind(this)
+        this.increaseClapCounts = this.increaseClapCounts.bind(this)
+        this.initializeClapAnimations = this.initializeClapAnimations.bind(this)
+        this.setHandStyleFilled = this.setHandStyleFilled.bind(this)
+        this.setClapCountText = this.setClapCountText.bind(this)
+        this.handleMouseDown = this.handleMouseDown.bind(this)
+        this.handleMouseUp = this.handleMouseUp.bind(this)
         this.init = this.init.bind(this)
         this.handleClapInterval = this.handleClapInterval.bind(this)
         this.handleIntervalClear = this.handleIntervalClear.bind(this)
     }
 
-    increaseClapCounter() {
-        this.totalClapCount++
-        this.tempClapCount++
-    }
+    increaseClapCounts() {
+        if (this.tempClapCount < 50) {
 
-    setHandsClapped() {
+            this.totalClapCount++
+            this.tempClapCount++
+        }
+    }
+    
+    setHandStyleFilled() {
         this.hands.style.fill = "#429835"
         this.hands.style.stroke = 'none'
         this.hands.style.animation = 'none'      
     }
-
-    setClapCounts() {
+    
+    setClapCountText() {
         this.totalClaps.textContent = this.totalClapCount
         this.clapCount.textContent = '+' + this.tempClapCount  
     }
+    
+    handleMouseDown() {
+        this.mouseDown = true
+        this.handleClapInterval()
+        if (this.clapInterval) clearInterval(this.clapInterval)
+        this.clapInterval = setInterval(this.handleClapInterval,200)
+    }
 
-    setClapAnimations() {
+    handleClapInterval() {
+        if(!this.mouseDown) {
+            clearInterval(this.clapInterval)
+            if (this.clapTimeout) clearTimeout(this.clapTimeout)
+            this.clapTimeout = setTimeout(this.handleIntervalClear, 500) 
+        } 
+        else {
+            this.increaseClapCounts()
+            this.setClapCountText()
+            if (this.totalClapCount > 0) this.setHandStyleFilled()    
+            this.initializeClapAnimations()
+        }
+    }
+
+    initializeClapAnimations() {
         this.clapCount.classList.remove('hidden', 'circle-pulse-clap')
         if (!this.tempClapCount) this.clapCount.classList.add('clap-count-enter')
         setTimeout(() => {                    
             this.clapCount.classList.add('circle-pulse-clap')
         }, 100)
     }
-
-    removeClapAnimations() {
-        this.clapCount.classList.remove('clap-count-enter')
+    
+    handleIntervalClear() {
+        this.clapCount.classList.remove('circle-pulse-clap', 'clap-count-enter')
+        clearInterval(this.clapInterval)           
         this.clapCount.classList.add('clap-count-exit')
-        setTimeout(() => {                    
+        setTimeout(() => {     
             this.clapCount.classList.remove('clap-count-exit')
             this.clapCount.classList.add('hidden')
+            this.tempClapCount = 0        
             this.clapCount.textContent = 0
-        }, 500)               
+        }, 100)               
     }
     
 
-    handleClapInterval() {
-        console.log("handle clap interval, ", this.mouseDown)
-        if(!this.mouseDown) {
-            "clap interval cleared"
-            clearInterval(this.clapInterval)
-            this.handleIntervalClear()
-        } else {
-            this.increaseClapCounter()
-            this.setClapCounts()
-            if (this.totalClapCount == 1) this.setHandsClapped()    
-            this.setClapAnimations()
-        }
-    }
-    handleIntervalClear() {
-        console.log("handle interval clear")
-        this.clapCount.classList.remove('circle-pulse-clap')
-        this.tempClapCount = 0
-        this.removeClapAnimations()
-    }
-
-    startClaps() {
-        this.mouseDown = true
-        this.handleClapInterval()
-        if (this.clapInterval) clearInterval(this.clapInterval)
-        this.clapInterval = setInterval(this.handleClapInterval,500)
-    }
-
-    stopClaps() {
-        console.log('mouseUp')
+    handleMouseUp() {
         this.mouseDown = false
     }
 
     init() {
-        this.clapCircle.addEventListener("mousedown", this.startClaps)
-        this.clapCircle.addEventListener("mouseup", this.stopClaps)               
+        this.clapCircle.addEventListener("mousedown", this.handleMouseDown)
+        this.clapCircle.addEventListener("mouseup", this.handleMouseUp)               
     }
 }
-
 
 
 const myClapper = new Clapper()
